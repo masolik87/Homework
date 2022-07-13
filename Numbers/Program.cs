f905bee4978e0b2e465165d09f41c1bad4ad13e0
Дмитрий <dimon20.07@mail.ru>
2022-07-14 00:57:59 +0300

    Numbers

---
+++ Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter the nubmer");
+int a = Convert.ToInt32(Console.ReadLine());
+if ((a / 2) * 2 == a)
+    {
+       Console.WriteLine("Yes");
+    }
+
+else
+{
+    Console.WriteLine("No");
+}
