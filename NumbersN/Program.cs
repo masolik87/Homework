7f9a7bc36e1f3c103b32b882553852d72d5031f4
Дмитрий <dimon20.07@mail.ru>
2022-07-14 01:25:32 +0300

    NumbersN

---
+++ NumbersN/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int rem = 1;
+while (rem <= num)
+{
+    if (rem % 2 == 0)
+    {
+        Console.Write(rem + " ");
+    }
+    rem++;
+}
